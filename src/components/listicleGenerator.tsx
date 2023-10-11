b43329cc2607bc906304e30f6540079dd2c71ae9
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-10-10 18:45:35 -0700

    Formatting

--- src/components/listicleGenerator.tsx
+++ src/components/listicleGenerator.tsx
@@ -11,15 +11,24 @@ export default function ListicleGenerator() {
         setRedditData(await response.json());
       }
 
+      const clearData = () => {
+        setRedditData([]);
+      }
+
       return (
-        <div>
-            <button className="btn w-44" onClick={fetchReddit}>Generate</button>
-            <h2>{redditData[0]}</h2>
-            {redditData.map((comment: any, index: number) => { 
-                if(index > 0) {
-                    return <p key={index}>{index}. {comment}</p>
-                }
-            })}
+        <div className="w-4/5">
+            <div className="flex flex-col items-center">
+                <button className="btn w-44 m-3" onClick={fetchReddit}>Generate</button>
+                <button className="btn w-20 m-3" onClick={clearData}>Clear</button>
+            </div>
+            <h2 className="text-lg py-5">{redditData[0]}</h2>
+            <div className="text-left">
+                {redditData.map((comment: any, index: number) => { 
+                    if(index > 0) {
+                        return <p className="p-1" key={index}>{index}. {comment}</p>
+                    }
+                })}
+            </div>
         </div>
       )
 }
